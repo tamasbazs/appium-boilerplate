d49490040255acedb56a16c3a44aecb8fe412ca9
tamasbazs <38215181+tamasbazs@users.noreply.github.com>
2021-10-12 02:29:51 +0200

    Update wdio.ios.app.conf.ts

--- config/wdio.ios.app.conf.ts
+++ config/wdio.ios.app.conf.ts
@@ -17,7 +17,7 @@ config.capabilities = [
     {
         // The defaults you need to have in your config
         platformName: 'iOS',
-        maxInstances: 1,
+        maxInstances: 2,
         // For W3C the appium capabilities need to have an extension prefix
         // This is `appium:` for all Appium Capabilities which can be found here
         // http://appium.io/docs/en/writing-running-appium/caps/
